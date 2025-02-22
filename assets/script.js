3a1240580927ac41aa63e5c6757ea79146740769
Dhileepguru <dhileepguru497@gmail.com>
2025-02-22 07:43:16 +0530

    Modified list Deletion

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 //alert("working");
+let inputValue=document.getElementById("inputValue");
+let ulList=document.getElementById("ulList");
+let listArr =[];
+
+function addList()
+{
+    if(inputValue.value != "")
+    {
+        listArr.push(inputValue.value);
+        ulList.innerHTML +="<li id="+(listArr.length-1)+"> <button onclick=deleteList("+(listArr.length-1)+")>"+inputValue.value+"</button> </li>"
+        inputValue.value="";
+    }
+}
+
+function deleteList(id)
+{
+  document.getElementById(id).style.display="none";
+}
