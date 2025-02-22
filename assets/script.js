cc4989d1e4a1fbafe4f1bc952e0bb590fa1c7194
Dhileepguru <dhileepguru497@gmail.com>
2025-02-22 08:20:44 +0530

    Inserted Local Storage

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,26 @@
 let inputValue=document.getElementById("inputValue");
 let ulList=document.getElementById("ulList");
 let listArr =[];
+let prevList;
+window.onload=()=>
+{
+    prevList=JSON.parse(localStorage.getItem("todosKey"));
+    addTodo(prevList);
+}
 
 function addList()
 {
     if(inputValue.value != "")
     {
         listArr.push(inputValue.value);
-        ulList.innerHTML +="<li id="+(listArr.length-1)+"> <button onclick=deleteList("+(listArr.length-1)+")>"+inputValue.value+"</button> </li>"
-        inputValue.value="";
+        localStorage.setItem("todosKey" , JSON.stringify(listArr));
+        addTodo(listArr);
     }
+}
+function addTodo(listArr)
+{
+    ulList.innerHTML +="<li id="+(listArr.length-1)+"> <button onclick=deleteList("+(listArr.length-1)+")>"+inputValue.value+"</button> </li>"
+    inputValue.value="";
 }
 
 function deleteList(id)
